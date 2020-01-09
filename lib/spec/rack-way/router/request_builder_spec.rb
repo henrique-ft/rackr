4f3b3ef46e4a9d9f380ecd0217fb1ab4fa142440
Henrique Fernandez Teixeira <hriqueft@gmail.com>
2020-01-09 15:14:20 -0300

    Add tests to RequestBuilder

--- lib/spec/rack-way/router/request_builder_spec.rb
+++ lib/spec/rack-way/router/request_builder_spec.rb
@@ -2,11 +2,34 @@ require_relative '../../../rack-way/router/request_builder.rb'
 require 'byebug'
 
 RSpec.describe Rack::Way::Router::RequestBuilder do
-  xit 'can build a request' do
+  it 'can build an Rack::Request' do
+    request = described_class.new({}).call
+    expect(request.class).to eq(Rack::Request)
   end
-  
+
   context 'given a route with params' do
-    xit 'it can match the route params' do
+    let(:rack_request) do
+      double(
+        path: '/test/1/test/2',
+        update_param: true
+      )
+    end
+
+    it 'it can match the route params' do
+      route =
+        Rack::Way::Router::Route
+          .new '/test/:param1/test/:param2', double(call: 'Hey test')
+
+      allow(Rack::Request)
+        .to(
+          receive(:new)
+          .and_return(rack_request)
+        )
+
+      expect(rack_request).to receive(:update_param).with(:param1, 1)
+      expect(rack_request).to receive(:update_param).with(:param2, 2)
+
+      described_class.new({}).call(route)
     end
   end
 end
