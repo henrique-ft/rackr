9e3f440e0f971dc068d65ce2d0cfe85ea9e0cf7b
Henrique F. Teixeira <hriqueft@gmail.com>
2023-08-12 09:34:53 -0300

    improve errors spec

--- lib/spec/rackr/router/errors_spec.rb
+++ lib/spec/rackr/router/errors_spec.rb
@@ -17,50 +17,54 @@ RSpec.describe Rackr::Router::Errors do
     end
   end
 
-  xdescribe 'check_path' do
+ describe 'check_path' do
     it do
       expect {
-        Rackr::Router::Errors.check_branch_name(1)
+        Rackr::Router::Errors.check_path(1)
       }.to raise_error(Rackr::Router::Errors::InvalidPathError)
 
       expect {
-        Rackr::Router::Errors.check_branch_name({})
+        Rackr::Router::Errors.check_path({})
       }.to raise_error(Rackr::Router::Errors::InvalidPathError)
     end
   end
 
-  xdescribe 'check_as' do
+  describe 'check_as' do
     it do
       expect {
-        Rackr::Router::Errors.check_branch_name(1)
+        Rackr::Router::Errors.check_as(1, '')
       }.to raise_error(Rackr::Router::Errors::InvalidNamedRouteError)
 
       expect {
-        Rackr::Router::Errors.check_branch_name({})
+        Rackr::Router::Errors.check_as({}, '')
       }.to raise_error(Rackr::Router::Errors::InvalidNamedRouteError)
     end
   end
 
-  xdescribe 'check_callbacks' do
+  describe 'check_callbacks' do
     it do
       expect {
-        Rackr::Router::Errors.check_branch_name(1)
+        Rackr::Router::Errors.check_callbacks([1], '')
       }.to raise_error(Rackr::Router::Errors::InvalidCallbackError)
 
       expect {
-        Rackr::Router::Errors.check_branch_name({})
+        Rackr::Router::Errors.check_callbacks([{}], '')
       }.to raise_error(Rackr::Router::Errors::InvalidCallbackError)
     end
   end
 
-  xdescribe 'check_endpoint' do
+  describe 'check_endpoint' do
     it do
       expect {
-        Rackr::Router::Errors.check_endpoint(1)
+        Rackr::Router::Errors.check_endpoint(1, '')
       }.to raise_error(Rackr::Router::Errors::InvalidEndpointError)
 
       expect {
-        Rackr::Router::Errors.check_endpoint({})
+        Rackr::Router::Errors.check_endpoint({} , '')
+      }.to raise_error(Rackr::Router::Errors::InvalidEndpointError)
+
+      expect {
+        Rackr::Router::Errors.check_endpoint(nil , '')
       }.to raise_error(Rackr::Router::Errors::InvalidEndpointError)
     end
   end
