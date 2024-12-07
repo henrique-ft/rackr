019f03950fcedeaeeb716ee2d99d6da0a439015f
henrique-ft <hriqueft@gmail.com>
2024-12-07 18:36:51 -0300

    update scaffold examples

--- scaffold/app/http/router.rb
+++ scaffold/app/http/router.rb
@@ -2,10 +2,10 @@ Router =
   Rackr.new(App::Config[:router]).call do
     get { text('hello world') }
 
-    r 'foods', as: :foods do
+    r 'foods' do
       get Actions::Foods::Index
 
-      r :id, as: :food, before: Callbacks::Foods::Assign do
+      r :id, before: Callbacks::Foods::Assign do
         get do |req|
           view 'foods/show', { food: req.food }
         end
