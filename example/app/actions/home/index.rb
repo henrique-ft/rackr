64fdd62336d05ec85e3803b02302385a0a0a43fe
henrique-ft <hriqueft@gmail.com>
2025-01-01 09:48:59 -0300

    remove comment

--- example/app/actions/home/index.rb
+++ example/app/actions/home/index.rb
@@ -11,17 +11,6 @@ module Actions
 
         view 'index'
       end
-
-      #def vieww(name, layoutt: 'layout')
-        #html (with_layout layoutt  do
-          #view(name)
-        #end)
-      #end
-
-      #def with_layout(layoutt, &block)
-        #base_path = config.dig(:views, :path) || 'views'
-        #erb(::File.read("#{base_path}/#{layoutt}.html.erb"), context: binding, &block)
-      #end
     end
   end
 end
