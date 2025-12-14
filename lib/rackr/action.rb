4b723642b4087ea4f452e72ccb0e9cb462a2f53c
Henrique F. Teixeira <hriqueft@gmail.com>
2025-12-14 11:40:52 -0300

    refactor internals default headers

--- lib/rackr/action.rb
+++ lib/rackr/action.rb
@@ -3,19 +3,37 @@
 require 'erubi'
 require 'oj'
 require 'rack'
-
 class Rackr
   module Action
+    @@default_headers_for = lambda { |content_type, headers, content|
+      {
+        'content-type' => "#{content_type}",
+        'content-length' => content.bytesize.to_s
+      }.merge(headers)
+    }
+
     @@render = {
       html: lambda do |val, status: 200, headers: {}, html: nil|
-        [status, { 'content-type' => 'text/html', 'content-length' => val.bytesize.to_s }.merge(headers), [val]]
+        [
+          status,
+          @@default_headers_for.('text/html', headers, val),
+          [val]
+        ]
       end,
       text: lambda do |val, status: 200, headers: {}, text: nil|
-        [status, { 'content-type' => 'text/plain', 'content-length' => val.bytesize.to_s }.merge(headers), [val]]
+        [
+          status,
+          @@default_headers_for.('text/plain', headers, val),
+          [val]
+        ]
       end,
       json: lambda do |val, status: 200, headers: {}, json: nil|
         val = Oj.dump(val, mode: :compat) unless val.is_a?(String)
-        [status, { 'content-type' => 'application/json', 'content-length' => val.bytesize.to_s }.merge(headers), [val]]
+        [
+          status,
+          @@default_headers_for.('application/json', headers, val),
+          [val]
+        ]
       end,
       res: lambda do |val, status: nil, headers: nil, res: nil|
         val.status = status if status
@@ -99,12 +117,11 @@ class Rackr
             return Rack::Response.new(
               parsed_erb,
               status,
-              { 'content-type' => 'text/html' }.merge(headers)
+              @@default_headers_for.('text/html', headers, parsed_erb)
             )
           end
 
-          [status, { 'content-type' => 'text/html', 'content-length' => parsed_erb.bytesize.to_s }.merge(headers),
-           [parsed_erb]]
+          [status, @@default_headers_for.('text/html', headers, parsed_erb), [parsed_erb]]
         end
 
         def load_json(val)
@@ -114,25 +131,16 @@ class Rackr
         end
 
         def html_response(content = '', status: 200, headers: {})
-          Rack::Response.new(content, status,
-                             { 'content-type' => 'text/html', 'content-length' => content.bytesize.to_s }.merge(headers))
+          Rack::Response.new(content, status, @@default_headers_for.('text/html', headers, content))
         end
 
         def json_response(content = {}, status: 200, headers: {})
           content = Oj.dump(content, mode: :compat) unless content.is_a?(String)
-          Rack::Response.new(
-            content,
-            status,
-            { 'content-type' => 'application/json', 'content-length' => content.bytesize.to_s }.merge(headers)
-          )
+          Rack::Response.new(content, status, @@default_headers_for.('application/json', headers, content))
         end
 
         def text_response(content, status: 200, headers: {})
-          Rack::Response.new(
-            content,
-            status,
-            { 'content-type' => 'text/plain', 'content-length' => content.bytesize.to_s }.merge(headers)
-          )
+          Rack::Response.new(content, status, @@default_headers_for.('text/plain', headers, content))
         end
 
         def load_erb(content, binding_context: nil)
