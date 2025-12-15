11d03a70c226017994ae404a9e6c12b62dcd12a4
Henrique F. Teixeira <hriqueft@gmail.com>
2025-12-14 21:31:57 -0300

    improve mime types

--- lib/rackr/action.rb
+++ lib/rackr/action.rb
@@ -6,7 +6,63 @@ require 'rack'
 
 class Rackr
   module Action
-    @@default_headers = lambda { |content_type, headers, content|
+    MIME_TYPES = {
+      text: "text/plain",
+      html: "text/html; charset=utf-8",
+      json: "application/json",
+      manifest: "text/cache-manifest",
+      atom: "application/atom+xml",
+      avi: "video/x-msvideo",
+      bmp: "image/bmp",
+      bz: "application/x-bzip",
+      bz2: "application/x-bzip2",
+      chm: "application/vnd.ms-htmlhelp",
+      css: "text/css",
+      csv: "text/csv",
+      flv: "video/x-flv",
+      gif: "image/gif",
+      gz: "application/x-gzip",
+      h264: "video/h264",
+      ico: "image/vnd.microsoft.icon",
+      ics: "text/calendar",
+      jpg: "image/jpeg",
+      js: "application/javascript",
+      mp4: "video/mp4",
+      mov: "video/quicktime",
+      mp3: "audio/mpeg",
+      mp4a: "audio/mp4",
+      mpg: "video/mpeg",
+      oga: "audio/ogg",
+      ogg: "application/ogg",
+      ogv: "video/ogg",
+      pdf: "application/pdf",
+      pgp: "application/pgp-encrypted",
+      png: "image/png",
+      psd: "image/vnd.adobe.photoshop",
+      rss: "application/rss+xml",
+      rtf: "application/rtf",
+      sh: "application/x-sh",
+      svg: "image/svg+xml",
+      swf: "application/x-shockwave-flash",
+      tar: "application/x-tar",
+      torrent: "application/x-bittorrent",
+      tsv: "text/tab-separated-values",
+      uri: "text/uri-list",
+      vcs: "text/x-vcalendar",
+      wav: "audio/x-wav",
+      webm: "video/webm",
+      wmv: "video/x-ms-wmv",
+      woff: "application/font-woff",
+      woff2: "application/font-woff2",
+      wsdl: "application/wsdl+xml",
+      xhtml: "application/xhtml+xml",
+      xml: "application/xml",
+      xslt: "application/xslt+xml",
+      yml: "text/yaml",
+      zip: "application/zip"
+    }.freeze
+
+    @@default_headers = proc { |content_type, headers, content|
       {
         'content-type' => content_type,
         'content-length' => content.bytesize.to_s
@@ -14,62 +70,56 @@ class Rackr
     }
 
     @@render = {
-      html: lambda do |val = '', status: 200, headers: {}, html: nil|
-        [
-          status,
-          @@default_headers.call('text/html; charset=utf-8', headers, val),
-          [val]
-        ]
-      end,
-      text: lambda do |val = '', status: 200, headers: {}, text: nil|
-        [
-          status,
-          @@default_headers.call('text/plain', headers, val),
-          [val]
-        ]
-      end,
-      json: lambda do |val = {}, status: 200, headers: {}, json: nil|
-        val = Oj.dump(val, mode: :compat) unless val.is_a?(String)
-        [
-          status,
-          @@default_headers.call('application/json', headers, val),
-          [val]
-        ]
-      end,
-      res: lambda do |val, status: nil, headers: nil, res: nil|
+      res: proc do |val, status: nil, headers: nil|
         val.status = status if status
         headers.each { |h, v| val.set_header(h, v) } if headers
         val.finish
       end,
-      response: lambda do |val, status: nil, headers: nil, response: nil|
+      response: proc do |val, status: nil, headers: nil|
         val.status = status if status
         headers.each { |h, v| val.set_header(h, v) } if headers
         val.finish
       end
-    }.freeze
+    }.merge(
+      MIME_TYPES.transform_values do |mime|
+        if mime == MIME_TYPES[:json]
+          proc do |val = '', status: 200, headers: {}|
+            val = Oj.dump(val, mode: :compat) unless val.is_a?(String)
+            [ status, @@default_headers.call(mime, headers, val), [val] ]
+          end
+        else
+          proc do |val = '', status: 200, headers: {}|
+            [ status, @@default_headers.call(mime, headers, val), [val] ]
+          end
+        end
+      end
+    ).freeze
 
     @@build_response = {
-      html: lambda do |val = '', status: 200, headers: {}, html: nil|
-        Rack::Response.new(val, status, @@default_headers.call('text/html; charset=utf-8', headers, val))
-      end,
-      text: lambda do |val = '', status: 200, headers: {}, text: nil|
-        Rack::Response.new(val, status, @@default_headers.call('text/plain', headers, val))
-      end,
-      json: lambda do |val = {}, status: 200, headers: {}, json: nil|
-        val = Oj.dump(val, mode: :compat) unless val.is_a?(String)
-        Rack::Response.new(val, status, @@default_headers.call('application/json', headers, val))
-      end,
-      head: lambda do |status, headers: {}, head: nil|
+      head: proc do |status, headers: {}|
         Rack::Response.new(nil, status, headers)
       end,
-      redirect_to: lambda do |url, headers: {}, redirect_to:|
+      redirect_to: proc do |url, headers: {}|
         Rack::Response.new(
           nil,
           302,
           { 'location' => url }.merge(headers)
         )
       end
-    }
+    }.merge(
+      MIME_TYPES.transform_values do |mime|
+        if mime == MIME_TYPES[:json]
+          proc do |val = '', status: 200, headers: {}|
+            val = Oj.dump(val, mode: :compat) unless val.is_a?(String)
+            Rack::Response.new(val, status, @@default_headers.call(mime, headers, val))
+          end
+        else
+          proc do |val = '', status: 200, headers: {}|
+            Rack::Response.new(val, status, @@default_headers.call(mime, headers, val))
+          end
+        end
+      end
+    ).freeze
 
     def self.included(base)
       base.class_eval do
@@ -87,15 +137,17 @@ class Rackr
         def render(**opts)
           type = opts.keys.first
           content = opts[type]
+          keyword_args = opts.except(type)
 
-          @@render[type]&.call(content, **opts) || _render_view(content, **opts)
+          @@render[type]&.call(content, **keyword_args) || _render_view(content, **keyword_args)
         end
 
         def build_response(**opts)
           type = opts.keys.first
           content = opts[type]
+          keyword_args = opts.except(type)
 
-          @@build_response[type]&.call(content, **opts) || _view_response(content, **opts)
+          @@build_response[type]&.call(content, **keyword_args) || _view_response(content, **keyword_args)
         end
 
         def _view_response(
@@ -124,7 +176,7 @@ class Rackr
         )
           base_path = config.dig(:views, :path) || 'views'
 
-          file_or_nil = lambda do |path|
+          file_or_nil = proc do |path|
             ::File.read(path)
           rescue Errno::ENOENT
             nil
