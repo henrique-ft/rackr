905509967991e1b2b7f5bc05c7299239af0e2323
Henrique F. Teixeira <hriqueft@gmail.com>
2025-12-23 11:00:16 -0300

    refactor for realworld example

--- examples/realworld/app/app.rb
+++ examples/realworld/app/app.rb
@@ -1,7 +1,7 @@
 App =
   Rackr.new(Config.get).call do
     get do
-      render html: '<h1> Realworld </h1>'
+      render html: '<h1>Real world </h1>'
     end
 
     scope 'api' do
@@ -20,44 +20,46 @@ App =
         end
       end
 
-      scope 'user', before: Callbacks::Users::Auth do
-        # Get Current User
-        # http GET localhost:4000/api/user Authorization:TOKEN
-        get { |req| render(json: { user: req.current_user.to_hash }) }
+      scope before: Callbacks::Users::Auth do
+        scope 'user' do
+          # Get Current User
+          # http GET localhost:4000/api/user Authorization:TOKEN
+          get { |req| render(json: { user: req.current_user.to_hash }) }
 
-        # Update Current User
-        # http PUT localhost:4000/api/user Authorization:TOKEN user[username]=ho
-        put do |req|
-          req.current_user.update(req.params["user"])
-          render json: { user: req.current_user.to_hash }
+          # Update Current User
+          # http PUT localhost:4000/api/user Authorization:TOKEN user[username]=ho
+          put do |req|
+            req.current_user.update(req.params["user"])
+            render json: { user: req.current_user.to_hash }
+          end
         end
-      end
 
-      scope 'profiles', before: Callbacks::Users::Auth do
-        scope :username, before: Callbacks::Users::AssignByUsername do
-          # Follow User
-          # http POST localhost:4000/api/profiles/:username/follow Authorization:TOKEN
-          post 'follow' do |req|
-            following = Follow[{user_id: user.id, follower_id: req.current_user.id}]
-            unless following
-              Follow.create(
-                {
-                  user_id: req.user.id,
-                  follower_id: req.current_user.id
-                }
-              )
+        scope 'profiles' do
+          scope :username, before: Callbacks::Users::AssignByUsername do
+            # Follow User
+            # http POST localhost:4000/api/profiles/:username/follow Authorization:TOKEN
+            post 'follow' do |req|
+              following = Follow[{user_id: req.user.id, follower_id: req.current_user.id}]
+              unless following
+                Follow.create(
+                  {
+                    user_id: req.user.id,
+                    follower_id: req.current_user.id
+                  }
+                )
+              end
+
+              render json: { profile: req.user.to_hash.merge({ following: true }) }
             end
 
-            render json: { profile: user.to_hash.merge({ following: true }) }
-          end
+            # Unfollow User
+            # http POST localhost:4000/api/profiles/:username/follow Authorization:TOKEN
+            delete 'follow' do |req|
+              follow = Follow[{ user_id: req.user.id, follower_id: req.current_user.id }]
+              follow&.destroy
 
-          # Unfollow User
-          # http POST localhost:4000/api/profiles/:username/follow Authorization:TOKEN
-          delete 'follow' do |req|
-            follow = Follow[{ user_id: req.user.id, follower_id: req.current_user.id }]
-            follow&.destroy
-
-            head 200
+              head 200
+            end
           end
         end
       end
