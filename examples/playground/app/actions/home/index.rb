71894cdb639f00da70ef849638798abb39e0c0a8
Henrique F. Teixeira <hriqueft@gmail.com>
2025-12-31 09:08:15 -0300

    update action callbacks examples

--- examples/playground/app/actions/home/index.rb
+++ examples/playground/app/actions/home/index.rb
@@ -5,6 +5,18 @@ module Actions
     class Index
       include Rackr::Action
 
+      before(proc do |req|
+        p 'before inside action'
+
+        req
+      end)
+
+      after(proc do |req|
+        p 'after inside action'
+
+        req
+      end)
+
       def call(_req)
         @name = "henrique"
         @title = "henrique"
