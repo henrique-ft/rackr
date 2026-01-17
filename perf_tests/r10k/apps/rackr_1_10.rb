84a475911312a348b4a0e02ccba7d6d7906e93b1
Henrique F. Teixeira <hriqueft@gmail.com>
2026-01-17 13:06:12 -0300

    update perf_tests color and builder

--- perf_tests/r10k/apps/rackr_1_10.rb
+++ perf_tests/r10k/apps/rackr_1_10.rb
@@ -2,33 +2,33 @@
 require_relative '../../../lib/rackr'
 App = Rackr.new.call do
   get '/a/:a' do |req|
-    render html: "4797-#{req.params[:a]}" 
+    render "4797-#{req.params[:a]}" 
   end
   get '/b/:a' do |req|
-    render html: "4798-#{req.params[:a]}" 
+    render "4798-#{req.params[:a]}" 
   end
   get '/c/:a' do |req|
-    render html: "4799-#{req.params[:a]}" 
+    render "4799-#{req.params[:a]}" 
   end
   get '/d/:a' do |req|
-    render html: "47100-#{req.params[:a]}" 
+    render "47100-#{req.params[:a]}" 
   end
   get '/e/:a' do |req|
-    render html: "47101-#{req.params[:a]}" 
+    render "47101-#{req.params[:a]}" 
   end
   get '/f/:a' do |req|
-    render html: "47102-#{req.params[:a]}" 
+    render "47102-#{req.params[:a]}" 
   end
   get '/g/:a' do |req|
-    render html: "47103-#{req.params[:a]}" 
+    render "47103-#{req.params[:a]}" 
   end
   get '/h/:a' do |req|
-    render html: "47104-#{req.params[:a]}" 
+    render "47104-#{req.params[:a]}" 
   end
   get '/i/:a' do |req|
-    render html: "47105-#{req.params[:a]}" 
+    render "47105-#{req.params[:a]}" 
   end
   get '/j/:a' do |req|
-    render html: "47106-#{req.params[:a]}" 
+    render "47106-#{req.params[:a]}" 
   end
 end
