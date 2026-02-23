23ee8469f45c2647a8cd43a5aabc3b80882c1914
Henrique F. Teixeira <hriqueft@gmail.com>
2026-02-22 21:52:05 -0300

    update templates

--- templates/complete/app/views/layout.rb
+++ templates/complete/app/views/layout.rb
@@ -5,12 +5,17 @@ module Views
     def layout(title:)
       html_layout do
         tag :head do
+          link rel: 'stylesheet', href: '/public/css/picnic.min.css'
+          link rel: 'stylesheet', href: '/public/css/app.min.css'
+
           tag :title, title
         end
 
         tag :body do
           yield
         end
+
+        script src: '/public/js/app.js'
       end
     end
   end
